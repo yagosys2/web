2ad85bd371a962ff60ab7215b81035ba0626c25f
yagosys2 <suhgr52@163.com>
2019-07-15 19:20:24 +0800

    add comments

--- python/asyncio/asyio_show_demo4.py
+++ python/asyncio/asyio_show_demo4.py
@@ -17,16 +17,15 @@ async def do_other_things():
     await custom_sleep('D',1)
 
 async def main():
-    loop = asyncio.get_running_loop()
+    loop = asyncio.get_running_loop() ###取得当前的event loop
 
     task = [
-    ask_sleep("A", 2),
-    ask_sleep("B", 6)
+    ask_sleep("A", 2), ###运行该任务, 在该任务block时交回控制权，执行下一个任务，等待回调
+    ask_sleep("B", 6) ###运行该任务，在该任务block时交回控制权。等待回调
     ]
-    await asyncio.wait(task)
+    await asyncio.wait(task)  ###将任务加入event loop,有资源就运行，直到运行完毕。执行下一行
+    await ask_sleep('C',5) ###暂停当前程序，运行ask_sleep,直到完成 
+    await do_other_things() ###暂停当前程序，运行do_other_things,直到完成
 
-    await do_other_things()
-    await ask_sleep('C',5)
-
-asyncio.run(main())
+asyncio.run(main()) ### 创建一个event loop , 并运行main())
 
